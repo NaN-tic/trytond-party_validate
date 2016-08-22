a88fd702b83257d80974d4faf37217814212b6b0
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-22 10:50:04 +0200

    Upgrade test to version 4.0

--- tests/test_party_validate.py
+++ tests/test_party_validate.py
@@ -2,22 +2,12 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_validate')
-
-    def test0005views(self):
-        'Test views'
-        test_view('party_validate')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'party_validate'
 
 
 def suite():
